91f6b6c62bc2fa0f1873f526065ebcf799bc4e9a
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2019-12-31 13:08:01 -0700

    extract Invoice

--- bannerpunk/onion_draw.py
+++ bannerpunk/onion_draw.py
@@ -7,36 +7,10 @@ from pyln.client import Millisatoshi
 
 from bannerpunk.pixel import Pixel
 from bannerpunk.hop_payload import BannerPunkHopPayload
+from bannerpunk.invoice import Invoice, SELF_PAYMENT
 
 from bolt.util import h2b
 from bolt.hop_payload import LegacyHopPayload, TlvHopPayload
-
-
-###############################################################################
-
-INVOICE_DESCRIPTION = ("circular payment invoice to deliver BannerPunk "
-                       "extension payload")
-SELF_PAYMENT = 1000 # in millisatoshis
-
-class Invoice:
-    def __init__(self, rpc):
-        self.rpc = rpc
-
-    def get_payment_secret(self, bolt11):
-        decoded = self.rpc.decodepay(bolt11)
-        return decoded['payment_secret']
-
-    def create_invoice(self):
-        try:
-            msatoshi = SELF_PAYMENT
-            label = str(uuid.uuid4())
-            description = INVOICE_DESCRIPTION
-            invoice = self.rpc.invoice(msatoshi, label, description)
-            invoice['payment_secret'] = self.get_payment_secret(
-                invoice['bolt11'])
-            return invoice, None
-        except:
-            return None, "could not create invoice"
 
 
 ###############################################################################
